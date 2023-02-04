8a6f243d1ef468c8baabeac8d4d9e4d85e6f2bc4
Cihan Çoban <cihan@cihancoban.com>
2023-02-04 09:56:39 +0300

    StoryManager Prefab & New Images

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using DefaultNamespace;
 using Gameplay;
 using Helpers;
 using UnityEngine;
--- Assets/Scripts/StoryManager.cs
+++ Assets/Scripts/StoryManager.cs
@@ -1,16 +1,29 @@
 using Helpers;
 using UnityEngine;
-
-namespace DefaultNamespace
-{
-    public class StoryManager : Singleton<StoryManager>
+public class StoryManager : Singleton<StoryManager>
     {
-        [SerializeField] private GameObject StartJourney;
-        
+        [SerializeField] private GameObject m_StartJourney;
+        [SerializeField] private GameObject m_EndingPath;
+        [SerializeField] private GameObject m_FirstEncounter;
+        [SerializeField] private GameObject m_SecondChances;
         public void StartJourneyPopup()
         {
-            Instantiate(StartJourney, transform.position, Quaternion.identity);
+            Instantiate(m_StartJourney, transform.position, Quaternion.identity);
+            Destroy(m_StartJourney, 5f);
         }
-
+        public void EndingPathPopup()
+        {
+            Instantiate(m_EndingPath, transform.position, Quaternion.identity);
+            Destroy(m_EndingPath, 5f);
+        }
+        public void FirstEncounterPopup()
+        {
+            Instantiate(m_FirstEncounter, transform.position, Quaternion.identity);
+            Destroy(m_FirstEncounter, 5f);
+        }
+        public void SecondChancesPopup()
+        {
+            Instantiate(m_SecondChances, transform.position, Quaternion.identity);
+            Destroy(m_SecondChances, 5f);
+        }
     }
-}
